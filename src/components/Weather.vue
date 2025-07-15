e2571d84539f668d09b571245919e4c0f61d2745
almina38 <alminabor@live.nl>
2025-07-16 00:58:15 +0200

    fix bug: geen error meer bij verkeerde stad, maar een popup

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container p-0">
-    <div class="d-flex">
+    <div v-if="errorMessage" class="alert alert-danger text-center mt-3">
+      {{ errorMessage }}
+    </div>
+
+    <div v-else class="d-flex">
       <div :style="backgroundStyle" class="card main-div w-100">
         <div class="p-3">
-        <div class="today-date-wrapper">
-        <h2 class="today">Today</h2>
-        <p class="text light date mb-0">{{ date }}</p>
-      </div>
-        <small class="time">{{ time }}</small>
+          <div class="today-date-wrapper">
+            <h2 class="today">Today</h2>
+            <p class="text light date mb-0">{{ date }}</p>
+          </div>
+          <small class="time">{{ time }}</small>
           <h2 class="place">
             <i class="fa fa-location"></i> {{ name }} <small>{{ country }}</small>
           </h2>
@@ -20,7 +24,7 @@
         </div>
       </div>
 
-      <div class="card card-2 w-100">
+      <div class="card card-2 w-100" v-if="!errorMessage">
         <table class="m-4">
           <tbody>
             <tr>
@@ -45,16 +49,15 @@
         <DaysWeather :cityname="cityname" />
 
         <div id="div_Form" class="m-3">
-  <form class="d-flex justify-content-center" action="">
-    <input
-      type="button"
-      value="Change Location"
-      @click="changeLocation"
-      class="btn change_btn btn-primary"
-    />
-  </form>
-</div>
-
+          <form class="d-flex justify-content-center" action="">
+            <input
+              type="button"
+              value="Change Location"
+              @click="changeLocation"
+              class="btn change_btn btn-primary"
+            />
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -86,6 +89,7 @@ export default {
       wind: null,
       humidity: null,
       country: null,
+      errorMessage: '',
       monthNames: [
         'January',
         'February',
@@ -109,9 +113,9 @@ export default {
   },
   computed: {
     windKmh() {
-    if (!this.wind) return '';
-    return Math.round(this.wind * 3.6) + ' km/h';
-  },
+      if (!this.wind) return ''
+      return Math.round(this.wind * 3.6) + ' km/h'
+    },
     backgroundStyle() {
       if (!this.iconUrl) return {}
 
@@ -155,44 +159,58 @@ export default {
     }
   },
   async created() {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=4ce1eea15ac17ef2217657495f8e4622`
-    )
-    const weatherData = response.data
-    this.temperature = Math.round(weatherData.main.temp)
-    this.description = weatherData.weather[0].description
-    this.name = weatherData.name
-    this.wind = weatherData.wind.speed
-    this.country = weatherData.sys.country
-    this.humidity = weatherData.main.humidity
-    this.temp_min = Math.round(weatherData.main.temp_min)
-    this.temp_max = Math.round(weatherData.main.temp_max)
-    this.iconUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=4ce1eea15ac17ef2217657495f8e4622`
+      )
+      const weatherData = response.data
+      this.temperature = Math.round(weatherData.main.temp)
+      this.description = weatherData.weather[0].description
+      this.name = weatherData.name
+      this.wind = weatherData.wind.speed
+      this.country = weatherData.sys.country
+      this.humidity = weatherData.main.humidity
+      this.temp_min = Math.round(weatherData.main.temp_min)
+      this.temp_max = Math.round(weatherData.main.temp_max)
+      this.iconUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
 
-    const utcNow = Math.floor(Date.now() / 1000);
-const localTimestamp = utcNow + weatherData.timezone; // offset in seconden
+      const utcNow = Math.floor(Date.now() / 1000)
+      const localTimestamp = utcNow + weatherData.timezone // offset in seconden
+      const localDate = new Date(localTimestamp * 1000)
 
-const localDate = new Date(localTimestamp * 1000);
+      this.date =
+        localDate.getUTCDate() +
+        ' ' +
+        this.monthNames[localDate.getUTCMonth()] +
+        ' ' +
+        localDate.getUTCFullYear()
 
-this.date =
-  localDate.getUTCDate() +
-  ' ' +
-  this.monthNames[localDate.getUTCMonth()] +
-  ' ' +
-  localDate.getUTCFullYear();
+      this.time =
+        localDate.getUTCHours().toString().padStart(2, '0') +
+        ':' +
+        localDate.getUTCMinutes().toString().padStart(2, '0')
 
-this.time =
-  localDate.getUTCHours().toString().padStart(2, '0') +
-  ':' +
-  localDate.getUTCMinutes().toString().padStart(2, '0');
-
+      this.errorMessage = '' // reset error
+    } catch (error) {
+      this.errorMessage = 'Please type the correct city name.'
+      // Clear previous data if any
+      this.temperature = null
+      this.description = null
+      this.name = null
+      this.wind = null
+      this.country = null
+      this.humidity = null
+      this.temp_min = null
+      this.temp_max = null
+      this.iconUrl = null
+      this.date = null
+      this.time = null
+    }
   }
 }
 </script>
 
 <style scoped>
-
-
 .weather-temp {
   margin: 0;
   font-weight: 700;
@@ -204,7 +222,7 @@ this.time =
   font-weight: 600;
 }
 
-.date{
+.date {
   font-size: 1.1rem;
 }
 
@@ -225,12 +243,12 @@ h2.today {
 .main-div {
   flex-basis: 55%;
   max-width: 55%;
-  backdrop-filter: blur(5px); 
+  backdrop-filter: blur(5px);
 }
 
 .card-2 {
   background: rgba(42, 42, 64, 0.85);
-  backdrop-filter: blur(5px); 
+  backdrop-filter: blur(5px);
   border-radius: 20px;
   max-height: 600px;
   flex-basis: 45%;
@@ -251,8 +269,8 @@ table {
 }
 
 .place i.fa-location {
-  font-size: 1rem; 
-  vertical-align: middle; 
+  font-size: 1rem;
+  vertical-align: middle;
   margin-right: 0.3rem;
 }
 
@@ -267,11 +285,12 @@ td {
 }
 
 .change_btn {
-background-color: #4fd1c5;
-color: #1e1e2f;
-border: none;
-border-radius: 8px;
-font-weight: 600;}
+  background-color: #4fd1c5;
+  color: #1e1e2f;
+  border: none;
+  border-radius: 8px;
+  font-weight: 600;
+}
 
 .change_btn:hover {
   transform: scale(0.98);
@@ -279,7 +298,14 @@ font-weight: 600;}
 }
 
 .today-date-wrapper {
-  display: block; 
+  display: block;
+}
+.alert {
+  background-color: #f44336;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 
 @media (max-width: 768px) {
@@ -302,15 +328,15 @@ font-weight: 600;}
   }
 
   .today-date-wrapper {
-    display: flex;      
-    gap: 0.5rem;        
-    align-items: center; 
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
   }
-table {
-  border-spacing: 0px 0px;
-}
+  table {
+    border-spacing: 0px 0px;
+  }
   table th,
-   table td {
+  table td {
     font-size: 13px;
     padding-left: 0;
     padding-right: 0;
@@ -352,23 +378,23 @@ table {
   }
 
   .weather-temp {
-    font-size: 2.5rem;  
+    font-size: 2.5rem;
   }
 
   h2.today {
     font-size: 1.8rem;
     margin-bottom: 0;
     display: inline-block;
-    vertical-align: middle; 
+    vertical-align: middle;
   }
 
   .date {
     font-size: 0.9rem;
     display: inline-block;
-    margin-left: 0.1rem; 
-    vertical-align: middle; 
+    margin-left: 0.1rem;
+    vertical-align: middle;
     position: relative;
-    top: 8px; 
+    top: 8px;
   }
 
   .p-3 {
@@ -376,7 +402,7 @@ table {
   }
 
   .time {
-    font-size: 1.1rem; 
+    font-size: 1.1rem;
     margin-bottom: 0.5rem;
     margin-left: 2px;
     display: block;
@@ -387,20 +413,20 @@ table {
   }
 
   .place {
-    font-size: 1.1rem;  
+    font-size: 1.1rem;
   }
 
   .temp h2 {
-    font-size: 1rem; 
+    font-size: 1rem;
   }
 
   .temp h2 img {
-    width: 40px;  
+    width: 40px;
     height: 40px;
   }
 
   .p-3 {
-    padding: 0.5rem !important; 
+    padding: 0.5rem !important;
   }
 }
 </style>
